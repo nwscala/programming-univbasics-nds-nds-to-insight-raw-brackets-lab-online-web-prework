8780d6cede2971cd8f4b9a3e2e8206b870e8e235
Noah Scala <tobi8912@gmail.com>
2019-11-26 21:12:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,23 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
-  #
+  
+  
+  # index_1 = 0
+  #total_hash = {}
+  #while index_1 < directors_database.count do
+  #  director_total = 0
+  #  index_2 = 0
+  #  while index_2 < directors_database[index_1][:movies].count do
+  #    director_total += directors_database[index_1][:movies][index_2][:worldwide_gross]
+  #    index_2 += 1
+  #  end
+  #  total_hash[directors_database[index_1][:name]] = director_total
+  #  index_1 += 1 
+  #end
+  #total_hash
+  
+  
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   # ...
