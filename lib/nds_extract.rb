0e8f48088241defca4caf9d1aef705af0592b6b1
Noah Scala <tobi8912@gmail.com>
2019-11-26 00:56:15 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,17 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  index_1 = 0
+  total_hash = {}
+  while index_1 < directors_database.count do
+    director_total = 0
+    index_2 = 0
+    while index_2 < directors_database[index_1][:movies].count do
+      director_total += directors_database[index_1][:movies][index_2][:worldwide_gross]
+      index_2 += 1
+    end
+    total_hash[directors_database[index_1][:name]] = director_total
+    index_1 += 1 
+  end
+  total_hash
 end
